2dbc1d076a9a23a5dca97779745c0723142b1753
ViktorMezei <123556135+ViktorMezei@users.noreply.github.com>
2024-01-21 19:11:45 +0100

    módosított Karácsonyfa v1.0

---
+++ src/karacsonyfa2/KaracsonyFa2.java
@@ -0,0 +1,53 @@
+package karacsonyfa2;
+
+
+public class KaracsonyFa2 {
+
+    
+    public static void main(String[] args) {
+        
+      
+     
+      int startIndex = 0;
+      
+      
+      String concat = " ";
+      
+      String treePart = "x";
+      
+      String treeExpansion = treePart+treePart;	  
+
+      
+      int treeHeight = 20;
+      
+      int middleIndex = treeHeight;
+      
+      
+     for (int j = 0; j<treeHeight; j++){ 
+     
+            for (int i = startIndex; i<middleIndex; i++){
+          
+                    
+                    System.out.print(concat);
+          
+                    if(i == middleIndex-1){
+          
+                    System.out.print(treePart);
+          
+                    }
+          
+          }
+          
+          treePart = treePart + treeExpansion;
+          
+          middleIndex -- ;
+          
+          System.out.println();
+        
+        
+        
+    }
+    
+  }
+  
+}
